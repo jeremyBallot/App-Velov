e5424cb459ca899dd6fbda3b12eb2dea1ceddce2
jeremyBallot <jerem@MacBook-Pro-de-jeremy.local>
2017-11-28 11:00:58 +0100

    Initial Commit

---
+++ JCDecauxProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JCDecauxProject
+//
+//  Created by jeremy ballot on 28/11/2017.
+//  Copyright © 2017 jeremy ballot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
